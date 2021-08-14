3f0617070a93eb03b98e53d2508485ad041c5135
j-panchenko <il586419001@gmail.com>
2021-08-15 01:13:50 +0300

    end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow left">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow right">'
+    });
 });
