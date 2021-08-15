8b642299b3ac7f3ee4816917789f67cab346a483
j-panchenko <il586419001@gmail.com>
2021-08-15 22:12:59 +0300

    added adaptability to the header

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function(){
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow left">',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow right">'
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('active');
+    });
 });
