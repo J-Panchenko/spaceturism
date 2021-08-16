5cdf7c7af8d092b8b66b78211106f61847bc5b84
j-panchenko <il586419001@gmail.com>
2021-08-16 15:49:14 +0300

    finished adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(function(){
     $('.slider__box').slick({
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow left">',
-        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow right">'
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow right">',
+        // responsive: [
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             arrows: false
+        //         }
+        //     }
+        // ]
     });
 
     $('.menu__btn').on('click', function(){
